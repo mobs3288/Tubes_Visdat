ebd6e755bcc7a1c63d5b7197977de25042c049aa
Ryan Oktaviandi Susilo Wibowo <49852465+mobs3288@users.noreply.github.com>
2023-06-13 22:40:20 +0700

    Update cause_of_death_graph.py

--- cause_of_death_graph.py
+++ cause_of_death_graph.py
@@ -117,4 +117,4 @@ if st.button('Update Plot'):
 html = file_html(plot, CDN, "Cause of Death Plot")
 
 # Menampilkan plot menggunakan komponen HTML
-st.components.v1.html(html, width=1200, height=600)
+st.components.v1.html(html, width=1800, height=400)
